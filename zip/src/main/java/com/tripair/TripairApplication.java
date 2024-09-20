78b7ed17ff8ac871647c567025f971f019c4fa36
Alef <lafitealef@gmail.com>
2024-09-20 16:48:12 -0300

    inicial commit

---
+++ zip/src/main/java/com/tripair/TripairApplication.java
@@ -0,0 +1,13 @@
+package com.tripair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripairApplication.class, args);
+	}
+
+}
